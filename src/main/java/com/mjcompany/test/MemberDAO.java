c46a3308f271c1644b0af906a1e739ec06038320
Minji287 <dpa357@gmail.com>
2023-04-12 10:00:00 +0900

    dao 클래스 추가

---
+++ src/main/java/com/mjcompany/test/MemberDAO.java
@@ -0,0 +1,50 @@
+package com.mjcompany.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class MemberDAO {
+	
+	private String driverName = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://localhost:3306/jspdb";
+	private String username = "root";
+	private String password = "1234";
+	
+	public int insertMember(String id, String pw, String email) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		String sql = "INSERT INTO members(id, pass, email) VALUES(?, ?, ?)";
+		
+		int dbFlag = 0;
+		
+		try {
+			Class.forName(driverName); // 드라이버 로딩
+			conn = DriverManager.getConnection(url, username, password); // 연결 생성
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, id);
+			pstmt.setString(2, pw);
+			pstmt.setString(3, email);
+
+			dbFlag = pstmt.executeUpdate();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return dbFlag;
+	}
+}
