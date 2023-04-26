491348cad68e213a661ca73f0c33bf7e801987d3
Minji287 <dpa357@gmail.com>
2023-04-26 11:59:30 +0900

    write, list, content_view jdbc로 변환

---
+++ src/main/java/com/mjcompany/board/dao/Constant/Constant.java
@@ -0,0 +1,9 @@
+package com.mjcompany.board.dao.Constant;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class Constant {
+	// static = 전역
+	public static JdbcTemplate template; // 전역 객체
+	
+}
