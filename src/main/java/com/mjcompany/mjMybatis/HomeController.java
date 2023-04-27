631480cb9b705dda75c2d24762fad3fbe6328904
Minji287 <dpa357@gmail.com>
2023-04-27 14:11:55 +0900

    글 삭제 기능 추가

--- src/main/java/com/mjcompany/mjMybatis/HomeController.java
+++ src/main/java/com/mjcompany/mjMybatis/HomeController.java
@@ -1,13 +1,17 @@
 package com.mjcompany.mjMybatis;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mjcompany.mjMybatis.dao.IDao;
+import com.mjcompany.mjMybatis.dto.BoardDto;
 
 /**
  * Handles requests for the application home page.
@@ -19,7 +23,13 @@ public class HomeController {
 	private SqlSession sqlSession; // sqlSession 빈이 컨테이너에서 자동 주입
 	
 	@RequestMapping(value = "/list")
-	public String list() {
+	public String list(Model model) {
+		
+		IDao dao = sqlSession.getMapper(IDao.class);
+		ArrayList<BoardDto> dtos = dao.listDao();
+		
+		model.addAttribute("list", dtos);
+		
 		return "list";
 	}
 	
@@ -35,6 +45,16 @@ public class HomeController {
 		
 		IDao dao = sqlSession.getMapper(IDao.class);
 		dao.writeDao(mwriter, mcontent);
+		
+		return "redirect:list";
+	}
+	
+	@RequestMapping(value = "/delete")
+	public String delete(HttpServletRequest request) {
+		String mid = request.getParameter("mid");
+		
+		IDao dao = sqlSession.getMapper(IDao.class);
+		dao.deleteDao(mid);
 		
 		return "redirect:list";
 	}
