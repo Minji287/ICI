1b58315298dbe79eaffe12181415953937ea9e8f
Minji287 <dpa357@gmail.com>
2023-05-03 16:14:58 +0900

    springboot test

---
+++ src/main/java/com/mjcompany/test/MjSpringbootTest20230503Application.java
@@ -0,0 +1,13 @@
+package com.mjcompany.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjSpringbootTest20230503Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjSpringbootTest20230503Application.class, args);
+	}
+
+}
