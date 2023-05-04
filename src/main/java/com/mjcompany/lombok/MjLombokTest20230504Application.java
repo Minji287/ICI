53b5d919680d10c780126cd80abfc8ab3f07e5a0
Minji287 <dpa357@gmail.com>
2023-05-04 11:58:55 +0900

    lombok 추가

---
+++ src/main/java/com/mjcompany/lombok/MjLombokTest20230504Application.java
@@ -0,0 +1,13 @@
+package com.mjcompany.lombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjLombokTest20230504Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjLombokTest20230504Application.class, args);
+	}
+
+}
