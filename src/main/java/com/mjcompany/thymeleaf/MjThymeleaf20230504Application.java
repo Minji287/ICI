73d5faab4b79312265a214fa02003a574e7385e7
Minji287 <dpa357@gmail.com>
2023-05-04 15:29:02 +0900

    model 데이터 출력 기능 추가

---
+++ src/main/java/com/mjcompany/thymeleaf/MjThymeleaf20230504Application.java
@@ -0,0 +1,13 @@
+package com.mjcompany.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjThymeleaf20230504Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjThymeleaf20230504Application.class, args);
+	}
+
+}
