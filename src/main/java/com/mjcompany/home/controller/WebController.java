452f5d6c2a5c7d89d31315893c34e52e37479dac
Minji287 <dpa357@gmail.com>
2023-05-10 17:36:22 +0900

    header 하이퍼링크 stylesheet 추가

--- src/main/java/com/mjcompany/home/controller/WebController.java
+++ src/main/java/com/mjcompany/home/controller/WebController.java
@@ -30,4 +30,9 @@ public class WebController {
 	public String contact() {
 		return "contact";
 	}
+	
+	@RequestMapping(value = "/question")
+	public String question() {
+		return "question";
+	}
 }
