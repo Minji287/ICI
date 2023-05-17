09421428df1980f1ea31093b0acb7139faa55958
Minji287 <dpa357@gmail.com>
2023-05-17 16:02:26 +0900

    rubato 설정 완료, 글 리스트에 댓글 개수 출력 기능 추가

---
+++ src/main/java/com/rubato/home/MjRubatoHomeProject20230517Application.java
@@ -0,0 +1,13 @@
+package com.rubato.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjRubatoHomeProject20230517Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjRubatoHomeProject20230517Application.class, args);
+	}
+
+}
